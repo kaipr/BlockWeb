126af3d4213e1d3daddb135aef0c501c65930b1f
Kai-Michael Pruess <kai.pruess@gmail.com>
2011-06-11 18:28:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlockWeb::Application.config.secret_token = 'a70e5cb15a622d28aee41c9352648dca43817840d848fb17351203748294414eb698556ea83a90e65120eb63f01517f7e5aa21367a988fbe5f1a21afb6f0490e'
